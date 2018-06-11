0176b4cc10f14c3b97fac03473b8c443b5d279cb
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-06-11 00:16:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,3 @@ function happyHolidays () {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo (firstName) {
-  return "Happy holidays," ${firstName}
-}
-
-function happyHolidayTo (holiday, name) {
-  return 'Happy ${holiday}, ${name}!'
-}
-
